a917141a4b01b6bafa4960ea60d659eb38faed4e
Wojciech Bojarski <bojawojt@gmail.com>
2020-01-07 15:41:38 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,16 +1,36 @@
-class Cat 
+# class Cat 
     
-    attr_accessor :name
+#     attr_accessor :name
 
-    def initialize(cats_name)
-        @name = cats_name
+#     def initialize(cats_name)
+#         @name = cats_name
+#     end
+    
+#     def meow
+#         puts "meow!"
+#     end
+
+# end
+
+# maru = Cat.new("Maru")
+
+# maru.meow
+
+
+class Cat
+    attr_accessor :name
+    def initialize(cat_name = "Maru")
+        
+        @name = cat_name
     end
+
     def meow
         puts "meow!"
     end
 
+
 end
 
-maru = Cat.new("maru")
-
+maru = Cat.new("Maru")
 maru.meow
+
